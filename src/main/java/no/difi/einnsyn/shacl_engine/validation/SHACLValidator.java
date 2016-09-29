aac1e6ebf3f9b3f8242ddc8e7fe1c2388787b4b8
Håvard Ottestad <haavard.ottestad@acando.no>
2016-09-29 20:21:24 +0200

    EIN-541 unit tests with embedded stardog. Use of community license cleared with Complexible.

--- src/main/java/no/difi/einnsyn/shacl_engine/validation/SHACLValidator.java
+++ src/main/java/no/difi/einnsyn/shacl_engine/validation/SHACLValidator.java
@@ -60,7 +60,6 @@ public class SHACLValidator {
 
             shapes = QueryResults
                 .stream(statements)
-                .parallel()
                 .map(statement -> new Shape(statement.getSubject(), shapesConnection, strictMode))
                 .collect(Collectors.toList());
         }
